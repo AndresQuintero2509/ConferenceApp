4dd5e4224ab981321bd3c79f05b601110b27b67e
Jesus Andres Sanchez Quintero <andresquintero2509>
2021-08-26 12:29:59 -0500

    Implementando Stereotype Annotations

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,24 +1,8 @@
-import com.conference.repository.HibernateSpeakerRepositoryImplementation;
-import com.conference.repository.SpeakerRepository;
-import com.conference.service.SpeakerService;
-import com.conference.service.SpeakerServiceImplementation;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.conference"})
 public class AppConfig {
 
-    @Bean(name = "speakerService")
-    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
-    public SpeakerService getSpeakerService() {
-        SpeakerServiceImplementation service = new SpeakerServiceImplementation(getSpeakerRepository());
-        return service;
-    }
-
-    @Bean(name = "speakerRepository")
-    public SpeakerRepository getSpeakerRepository() {
-        return new HibernateSpeakerRepositoryImplementation();
-    }
 }
