f0e8104408c5a9b0cdb8888c8035bb49cb03b9f8
Jesus Andres Sanchez Quintero <andresquintero2509>
2021-08-26 11:21:05 -0500

    Configuracion usando Singleton

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,16 @@ import com.conference.repository.HibernateSpeakerRepositoryImplementation;
 import com.conference.repository.SpeakerRepository;
 import com.conference.service.SpeakerService;
 import com.conference.service.SpeakerServiceImplementation;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImplementation service = new SpeakerServiceImplementation(getSpeakerRepository());
         return service;
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,6 +7,9 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service);
         System.out.println(service.findAll().get(0).getFirstName());
+        SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
     }
 }
